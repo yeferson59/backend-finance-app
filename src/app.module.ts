58c15041884f50f459fb7429f020be4025a4b903
yefersonT <116679774+yeferson59@users.noreply.github.com>
2024-09-10 01:12:29 -0500

    add new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { MarketDataModule } from './modules/market-data/market-data.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+  }), PrismaModule, MarketDataModule, UsersModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
