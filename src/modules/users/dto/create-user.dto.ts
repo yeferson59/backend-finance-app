422b50b071bdd25370c83c1b03f5cb57997d7f8c
yefersonT <116679774+yeferson59@users.noreply.github.com>
2024-09-10 15:42:17 -0500

    created field of crete user

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,34 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength, IsOptional } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(25)
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(25)
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(25)
+  username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsOptional()
+  role: string | null;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/, { message: 'La contraseña debe tener al menos una letra mayuscaula, una minuscula y un numero' })
+  @MaxLength(22, { message: 'Debe contener menos de 8 caracteres de longitud' })
+  password: string;
+}
