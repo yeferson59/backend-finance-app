11f88e5c580e62302484b9711569d0cddbc421fd
yefersonT <116679774+yeferson59@users.noreply.github.com>
2024-09-10 15:51:52 -0500

    add interface of user with name role

---
+++ src/modules/users/interfaces/user.interface.ts
@@ -0,0 +1,22 @@
+import { User as UserDb } from '@prisma/client'
+
+export interface User {
+  id: string;
+  name: string | null;
+  lastName: string | null;
+  username: string | null;
+  password: string | null;
+  email: string;
+  emailVerified: Date | null;
+  image: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+  role: string;
+}
+
+export interface UserRoleDb extends UserDb {
+  role: {
+    id: number,
+    name: string
+  }
+} 
