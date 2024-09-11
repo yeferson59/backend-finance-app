4e9c2aa3d91352d03bd2097563a38b7f5258bf46
AnonymusDev <unipamplona44@gmail.com>
2024-09-11 17:13:36 -0500

    remove unnessary module

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,14 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SignUpDto } from './dto/sign-up.dto';
-import { UsersService } from '../users/users.service';
 
 @Controller('auth')
 export class AuthController {
 
-  constructor(private readonly usersService: UsersService) { }
-
   @Post('sign-up')
   signUp(@Body() signUpDto: SignUpDto) {
-    return this.usersService.create(signUpDto);
+    return "auth";
   }
 }
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersModule } from '../users/users.module';
-import { UsersService } from '../users/users.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, UsersService],
-  imports: [UsersModule]
+  providers: [AuthService],
 })
 export class AuthModule { }
