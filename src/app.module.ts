c3ab8c46d650b283975bbeee09dfbef624ef7ce9
yefersonT <116679774+yeferson59@users.noreply.github.com>
2024-09-19 16:23:09 -0500

    add module of schedule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,19 @@ import { PrismaModule } from './prisma/prisma.module';
 import { MarketDataModule } from './modules/market-data/market-data.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true
-  }), PrismaModule, MarketDataModule, UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    PrismaModule,
+    MarketDataModule,
+    UsersModule,
+    AuthModule,
+    ScheduleModule.forRoot()
+  ],
   controllers: [],
   providers: [],
 })
